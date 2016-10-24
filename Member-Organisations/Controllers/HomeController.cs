0388b182537e29ffdf37400527a212c6d3c2d5e4
lina-hs <lina.hannersanchez@gmail.com>
2016-10-24 12:39:20 +0200

    Set register member view to default view

--- Member-Organisations/Controllers/HomeController.cs
+++ Member-Organisations/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Member_Organisations.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View("RegisterMember");
         }
     }
 }
