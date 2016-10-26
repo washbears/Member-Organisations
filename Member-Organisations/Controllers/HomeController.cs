df0a2848fc33fe98ff851226e2aa5edf94279b02
lina-hs <lina.hannersanchez@gmail.com>
2016-10-26 14:39:35 +0200

    Revert "Added code in homecontroller and member/organisation classes to find and deliver data from DB to RegisterOrganisation."
    This reverts commit ae081f77ea2654e8a941adcd9fc38c149bac7dca.

--- Member-Organisations/Controllers/HomeController.cs
+++ Member-Organisations/Controllers/HomeController.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Member_Organisations.Models;
 
 namespace Member_Organisations.Controllers
 {
     public class HomeController : Controller
     {
-
-        MODatabaseEntities OrgName = new MODatabaseEntities();
-
         // GET: Home
         public ActionResult Index()
         {
@@ -27,7 +23,7 @@ namespace Member_Organisations.Controllers
         // GET: Register Organisation
         public ActionResult RegisterOrganisation()
         {
-            return View(new MODatabaseEntities().Organisations.ToList());
+            return View();
         }
     }
 }
