f13e49a1d9a4a4e51b35ae28f8fe3db23c4fa102
lina-hs <lina.hannersanchez@gmail.com>
2016-10-27 11:35:25 +0200

    Created ViewModel classes for Index and RegisterMember views

---
+++ Member-Organisations/ViewModels/RegisterMemberViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Member_Organisations.ViewModels
+{
+    public class RegisterMemberViewModel
+    {
+        public List<Organisation> orgs { get; set;  }
+        public Member member { get; set;  }
+
+        public RegisterMemberViewModel()
+        {
+            orgs = new List<Organisation>();
+            member = new Member();
+        }
+        public RegisterMemberViewModel(List<Organisation> orgs)
+        {
+            this.orgs = orgs;
+            member = new Member();
+        }
+        public RegisterMemberViewModel(Member member)
+        {
+            orgs = new List<Organisation>();
+            this.member = member;
+        }
+        public RegisterMemberViewModel(List<Organisation> orgs, Member member)
+        {
+            this.orgs = orgs;
+            this.member = member;
+        }
+
+    }
+}
